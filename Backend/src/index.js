fc3cac9c9d8acdf2482454fca3c4204258a9f8a4
uttam verma <uvbtechs2025@gmail.com>
2026-06-18 18:00:16 +0530

    Add Dockerfile and update index.js for static file serving

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,6 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import User from './models/user.model.js'
+
+import fs from 'fs'
+import path from 'path'
+
 import { connectDB } from './lib/db.js'
 import { clerkMiddleware } from '@clerk/express'
 
@@ -9,11 +13,20 @@ import { clerkMiddleware } from '@clerk/express'
 
 const PORT = process.env.PORT
 const FRONTEND_URL = process.env.FRONTEND_URL
+const PublicDir = path.join(process.cwd(),"public")
+
 const app  = express()
 
 app.use(express.json())
 app.use(clerkMiddleware())
 app.use(cors({origin:FRONTEND_URL,credentials:true}))
+
+if(fs.existsSync(PublicDir)){// only for deployment 
+    app.use(express.static(PublicDir))
+    app.get('/{*any}',(req,res,next)=>{
+        res.sendFile(path.join(PublicDir,"index.html"),(err)=>next(err))
+    })
+}
 
 
 app.listen(PORT,()=>{
