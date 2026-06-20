e71f003f8b95b667fc80716cecabae87a9a12b60
uttam verma <uvbtechs2025@gmail.com>
2026-06-20 23:35:31 +0530

    missing import

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,37 +1,38 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import User from './models/user.model.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import User from "./models/user.model.js";
+import clerkWebhook from "./routes/clerkWebhook.route.js";
+import fs from "fs";
+import path from "path";
 
-import fs from 'fs'
-import path from 'path'
+import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
-import { connectDB } from './lib/db.js'
-import { clerkMiddleware } from '@clerk/express'
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+const PublicDir = path.join(process.cwd(), "public");
 
+const app = express();
 
+app.use(
+  "/api/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhook,
+);
+app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(clerkMiddleware());
 
-const PORT = process.env.PORT
-const FRONTEND_URL = process.env.FRONTEND_URL
-const PublicDir = path.join(process.cwd(),"public")
-
-
-const app  = express()
-
-app.use("/api/webhooks/clerk",express.raw({type: "application/json"}),clerkWebhook)
-app.use(express.json())
-app.use(cors({origin:FRONTEND_URL,credentials:true}))
-app.use(clerkMiddleware())
-
-if(fs.existsSync(PublicDir)){// only for deployment 
-    app.use(express.static(PublicDir))
-    app.get('/{*any}',(req,res,next)=>{
-        res.sendFile(path.join(PublicDir,"index.html"),(err)=>next(err))
-    })
+if (fs.existsSync(PublicDir)) {
+  // only for deployment
+  app.use(express.static(PublicDir));
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(PublicDir, "index.html"), (err) => next(err));
+  });
 }
 
-
-app.listen(PORT,()=>{
-    connectDB()
-    console.log(`server is running  at port ${PORT}`)
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`server is running  at port ${PORT}`);
+});
