9f3f700b1045cd8efb8fe1f804adca7120bb1fd6
uttam verma <uvbtechs2025@gmail.com>
2026-06-20 23:30:08 +0530

    Add Clerk webhook handling for user events in new clerk.webhooks.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,8 +15,10 @@ const PORT = process.env.PORT
 const FRONTEND_URL = process.env.FRONTEND_URL
 const PublicDir = path.join(process.cwd(),"public")
 
+
 const app  = express()
 
+app.use("/api/webhooks/clerk",express.raw({type: "application/json"}),clerkWebhook)
 app.use(express.json())
 app.use(cors({origin:FRONTEND_URL,credentials:true}))
 app.use(clerkMiddleware())
