422205ebf14817269fdaa1ab341fb399285c60c9
uttam verma <uvbtechs2025@gmail.com>
2026-06-27 23:58:01 +0530

    Add conversation retrieval and message fetching for sidebar

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,12 +1,76 @@
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
-export async function getUsersForSidebar(req,res) {
-    try {
-        const loggedInUser = req.user._id
-       const filteredUsers = await User.find({_id: {$ne : loggedInUser}} ).select("-clerkId" )
-       res.status(200).json(filteredUsers)
-    } catch (error) {
-        console.error("error in Getuserfromsidebar: ", error.message)
-        res.status(500).json({message:"Internal server error"})
-    }
+export async function getUsersForSidebar(req, res) {
+  try {
+    const loggedInUser = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUser },
+    }).select("-clerkId");
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("error in Getuserfromsidebar: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
+
+export async function getConversationForSidebar(req, res) {
+  try {
+    const loggedInUser = req.user._id;
+    const conversations = await Message.aggregate([
+      {
+        $match: {
+          $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $cond: [
+              { $eq: ["$senderId", loggedInUserId] },
+              "$receiverId",
+              "$senderId",
+            ],
+          },
+          lastMessageAt: { $max: "$createdAt" },
+        },
+      },
+      { $sort: { lastMessageAt: -1 } },
+      {
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      { $replaceRoot: { newRoot: { $first: "$user" } } },
+      { $project: { clerkId: 0 } },
+    ]);
+     res.status(200).json(conversations);
+  } catch (error) {
+    console.error("Error in getConversationsForSidebar:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function getMessage(req,res) {
+      try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    }).sort({ createdAt: 1 });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error in getMessages:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+
--- Backend/src/routes/message.routes.js
+++ Backend/src/routes/message.routes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import {getUsersForSidebar} from '../controllers/message.controller.js'
+import {getUsersForSidebar , getConversationForSidebar, getMessage} from '../controllers/message.controller.js'
 import { protectRoute } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
 router.get('/users',protectRoute, getUsersForSidebar)
+router.get('/conversations',protectRoute, getConversationForSidebar)
+router.get('/:id',protectRoute, getMessage)
 
 export default router
