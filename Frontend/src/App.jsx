426bcf1d6b7e1e4638301bb78e88b2d875003b92
uttam verma <uvbtechs2025@gmail.com>
2026-07-01 15:42:10 +0530

    feat: implement theme and wallpaper context providers
    - Added ThemeContext and WallpaperContext to manage application themes and wallpapers.
    - Created herouiThemePresets.js and wallpapers.js for theme presets and wallpaper data.
    - Introduced ThemeProvider and WallpaperProvider components to wrap the application.
    - Updated main.jsx to include BrowserRouter for routing.
    - Created AuthPage and ChatPage components for future use.
    - Removed old CSS variables and integrated Tailwind CSS and HeroUI styles.
    - Added heroui-theme-presets.css for theme preset styles.
    - Deleted unused vite.svg asset.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,24 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import './App.css'
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
 
 function App() {
-
+  const {isSignedIn , isLoaded} = useAuth();
+  if(!isLoaded) return <p>Loading ....</p>
 
   return (
-  <div>
-   
-     <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-  </div>
-  )
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage />: <Navigate to={"/auth"} replace/>} />
+          <Route path="/auth" element={!isSignedIn ? <AuthPage />: <Navigate to={"/"} replace/>} />
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/react";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
-)
+);
